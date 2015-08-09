ae0d26a0524d5d3cbc8ad1325d8f123009d690e5
unknown <jaishree.taware@gmail.com>
2015-08-08 22:10:43 -0700

    Added comments to code file

--- plot1.R
+++ plot1.R
@@ -1,7 +1,18 @@
+# Install the sqldf package which will help us filter out data initiall while reading from the file
 install.packages("sqldf")
 library(sqldf)
+
+# Read the data from the text file into data frame using sql query to filter on date
 data <- read.csv.sql( file ="household_power_consumption.txt", header = TRUE, sep=";", sql = "select * from file where Date in ('1/2/2007','2/2/2007')")
+
+# Add a DateTime column and compute the values by appling the strptime function on Date andTime column
 data$DateTime = strptime(x = paste( data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
+
+# Invoke the PNG device
 png( filename = "plot1.png", width=480, height = 480, units = "px")
+
+# Plot the histogram as per the requirement
 hist(x=data$Global_active_power, col = "RED", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+# Close the device so that the plot is saved as a png file
 dev.off()
