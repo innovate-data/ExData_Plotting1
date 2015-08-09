5304430ec68f142480b132c556cc9cea04724bf1
unknown <jaishree.taware@gmail.com>
2015-08-08 21:58:13 -0700

    Plot 1 code file

---
+++ plot1.R
@@ -0,0 +1,7 @@
+install.packages("sqldf")
+library(sqldf)
+data <- read.csv.sql( file ="household_power_consumption.txt", header = TRUE, sep=";", sql = "select * from file where Date in ('1/2/2007','2/2/2007')")
+data$DateTime = strptime(x = paste( data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
+png( filename = "plot1.png", width=480, height = 480, units = "px")
+hist(x=data$Global_active_power, col = "RED", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
